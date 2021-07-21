14c8f3f08df650f5ea4df52e215955098787281c
Денис Кузьменко <maidadur@gmail.com>
2021-07-21 11:54:17 +0300

    # fixed data load with paging and ordering

--- Maid.Core/DB/EntityRepository.cs
+++ Maid.Core/DB/EntityRepository.cs
@@ -10,11 +10,11 @@
 
 	public class EntityRepository : IEntityRepository
 	{
-		protected DbContext Context { get; set; }
-
 		public EntityRepository(DbContext repositoryContext) {
 			Context = repositoryContext;
 		}
+
+		protected DbContext Context { get; set; }
 
 		public async Task<IEnumerable<BaseEntity>> GetAllAsync(Type type) {
 			return await Context.Set(type).ToListAsync();
@@ -23,58 +23,11 @@
 
 	public class EntityRepository<TEntity> : IEntityRepository<TEntity> where TEntity : BaseEntity
 	{
-		protected DbContext Context { get; set; }
-
 		public EntityRepository(DbContext repositoryContext) {
 			Context = repositoryContext;
 		}
 
-		public async Task<IEnumerable<TEntity>> GetAllAsync(SelectOptions options = null) {
-			options = options ?? new SelectOptions();
-			var dbSet = Context.Set<TEntity>();
-			if (options.LoadLookups) {
-				return await dbSet
-					.Include(Context)
-					.ApplyOffsetOptions(options)
-					.ApplyOrderOptions(options.OrderOptions)
-					.ToListAsync();
-			}
-			return await dbSet
-				.ApplyOffsetOptions(options)
-				.ApplyOrderOptions(options.OrderOptions)
-				.ToListAsync();
-		}
-
-		public IEnumerable<TEntity> GetAll(SelectOptions options = null) {
-			options = options ?? new SelectOptions();
-			var dbSet = Context.Set<TEntity>();
-			if (options.LoadLookups) {
-				return dbSet
-					.Include(Context)
-					.ApplyOffsetOptions(options)
-					.ApplyOrderOptions(options.OrderOptions)
-					.ToList();
-			}
-			return dbSet
-				.ApplyOffsetOptions(options)
-				.ApplyOrderOptions(options.OrderOptions)
-				.ToList();
-		}
-
-		public async Task<IEnumerable<TEntity>> GetByAsync(Expression<Func<TEntity, bool>> expression, SelectOptions options = null) {
-			options = options ?? new SelectOptions();
-			return await Context.Set<TEntity>()
-				.ApplyOrderOptions(options.OrderOptions)
-				.Where(expression)
-				.ApplyOffsetOptions(options)
-				.ToListAsync();
-		}
-
-		public IEnumerable<TEntity> GetBy(Expression<Func<TEntity, bool>> expression) {
-			return Context.Set<TEntity>()
-				.Where(expression)
-				.ToList();
-		}
+		protected DbContext Context { get; set; }
 
 		public void Create(TEntity entity) {
 			if (entity.CreatedOn == DateTime.MinValue) {
@@ -82,10 +35,6 @@
 			}
 			Context.Attach(entity);
 			Context.Set<TEntity>().Add(entity);
-		}
-
-		public void Update(TEntity entity) {
-			Context.Set<TEntity>().Update(entity);
 		}
 
 		public void Delete(TEntity entity) {
@@ -110,18 +59,69 @@
 				.SingleOrDefault(item => item.Id == id);
 		}
 
+		public IEnumerable<TEntity> GetAll(SelectOptions options = null) {
+			options = options ?? new SelectOptions();
+			var dbSet = Context.Set<TEntity>();
+			if (options.LoadLookups) {
+				return dbSet
+					.Include(Context)
+					.ApplyOrderOptions(options.OrderOptions)
+					.ApplyOffsetOptions(options)
+					.ToList();
+			}
+			return dbSet
+				.ApplyOrderOptions(options.OrderOptions)
+				.ApplyOffsetOptions(options)
+				.ToList();
+		}
+
+		public async Task<IEnumerable<TEntity>> GetAllAsync(SelectOptions options = null) {
+			options = options ?? new SelectOptions();
+			var dbSet = Context.Set<TEntity>();
+			if (options.LoadLookups) {
+				return await dbSet
+					.Include(Context)
+					.ApplyOrderOptions(options.OrderOptions)
+					.ApplyOffsetOptions(options)
+					.ToListAsync();
+			}
+			return await dbSet
+				.ApplyOrderOptions(options.OrderOptions)
+				.ApplyOffsetOptions(options)
+				.ToListAsync();
+		}
+
 		public Task<TEntity> GetAsync(Guid id) {
 			return Context.Set<TEntity>().AsQueryable()
 				.Include(Context)
 				.SingleOrDefaultAsync(item => item.Id == id);
 		}
 
-		public async Task SaveAsync() {
-			await Context.SaveChangesAsync();
+		public IEnumerable<TEntity> GetBy(Expression<Func<TEntity, bool>> expression) {
+			return Context.Set<TEntity>()
+				.Where(expression)
+				.ToList();
+		}
+
+		public async Task<IEnumerable<TEntity>> GetByAsync(Expression<Func<TEntity, bool>> expression, SelectOptions options = null) {
+			options = options ?? new SelectOptions();
+			return await Context.Set<TEntity>()
+				.ApplyOrderOptions(options.OrderOptions)
+				.Where(expression)
+				.ApplyOffsetOptions(options)
+				.ToListAsync();
 		}
 
 		public void Save() {
 			Context.SaveChanges();
+		}
+
+		public async Task SaveAsync() {
+			await Context.SaveChangesAsync();
+		}
+
+		public void Update(TEntity entity) {
+			Context.Set<TEntity>().Update(entity);
 		}
 	}
 }
