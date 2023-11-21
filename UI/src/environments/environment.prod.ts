6e603b82a5dd13cae8de0bf76748fb253253170b
D.Kuzmenko <D.Kuzmenko@creatio.com>
2023-11-21 13:10:15 +0200

    # updated auth datafor prod env

--- UI/src/environments/environment.prod.ts
+++ UI/src/environments/environment.prod.ts
@@ -4,11 +4,16 @@ export const environment = {
 	authHost: '',
 	notificationsHost: "https://push.{domain}",
 	auth: {
-		flowName: "B2C_1_maid-dev",
+		flowName: "B2C_1_maid-prod",
 		authorityDomain: "maidadur.b2clogin.com",
-		authority: "https://maidadur.b2clogin.com/maidadur.onmicrosoft.com/B2C_1_maid-dev",
-		clientId: '737a7083-5747-4b3f-8ffc-dff956a240d2',
-		scopes: ["https://maidadur.onmicrosoft.com/737a7083-5747-4b3f-8ffc-dff956a240d2"],
+		authority: "https://maidadur.b2clogin.com/maidadur.onmicrosoft.com/B2C_1_maid-prod",
+		clientId: 'd213892c-7e41-45ab-be19-4d0028fa5468',
+		ui_scopes: ["https://maidadur.onmicrosoft.com/d213892c-7e41-45ab-be19-4d0028fa5468"],
+		content_monitor_scopes: [
+			"https://maidadur.onmicrosoft.com/feb7657b-a552-4ba3-bdb5-5d8b0c85d4db/tasks.write", 
+			"https://maidadur.onmicrosoft.com/feb7657b-a552-4ba3-bdb5-5d8b0c85d4db/tasks.read"
+		],
+		redirectUri: 'https://dwork.me'
 	}
   };
   
