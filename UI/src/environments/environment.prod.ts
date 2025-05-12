4616561657c433a76a515c8d053abc24c5863882
D.Kuzmenko <D.Kuzmenko@creatio.com>
2025-05-12 20:55:00 +0300

    - fix env

--- UI/src/environments/environment.prod.ts
+++ UI/src/environments/environment.prod.ts
@@ -10,7 +10,7 @@ export const environment = {
 		authority: "https://maidadur.b2clogin.com/maidadur.onmicrosoft.com/B2C_1_maid-prod",
 		clientId: 'd213892c-7e41-45ab-be19-4d0028fa5468',
 		ui_scopes: ["https://maidadur.onmicrosoft.com/d213892c-7e41-45ab-be19-4d0028fa5468"],
-		content_monitor_scopes: [
+		app_scopes: [
 			"https://maidadur.onmicrosoft.com/feb7657b-a552-4ba3-bdb5-5d8b0c85d4db/tasks.write", 
 			"https://maidadur.onmicrosoft.com/feb7657b-a552-4ba3-bdb5-5d8b0c85d4db/tasks.read"
 		],
