5ca9764befdd99f345bdd227cf8f90b396a79ec0
D.Kuzmenko <D.Kuzmenko@creatio.com>
2025-05-12 21:54:57 +0300

    -- migration

---
+++ Maid.Binance.DB/Migrations/20250512185432_BinanceOrderImageUrlFix.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Maid.Binance.DB.Migrations
+{
+    /// <inheritdoc />
+    public partial class BinanceOrderImageUrlFix : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Notes",
+                table: "BinanceOrders",
+                type: "longtext",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "longtext")
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ImageUrl",
+                table: "BinanceOrders",
+                type: "longtext",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "longtext")
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "BinanceOrders",
+                keyColumn: "Notes",
+                keyValue: null,
+                column: "Notes",
+                value: "");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Notes",
+                table: "BinanceOrders",
+                type: "longtext",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "longtext",
+                oldNullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.UpdateData(
+                table: "BinanceOrders",
+                keyColumn: "ImageUrl",
+                keyValue: null,
+                column: "ImageUrl",
+                value: "");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ImageUrl",
+                table: "BinanceOrders",
+                type: "longtext",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "longtext",
+                oldNullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+        }
+    }
+}
