c44d7f786710929a7b11d53cd7c429a56f15db53
D.Kuzmenko <D.Kuzmenko@creatio.com>
2025-05-13 13:02:30 +0300

    -- reworked trades loading logic

--- Maid.Binance/BinanceTradesLoader/BinanceTradesLoader.cs
+++ Maid.Binance/BinanceTradesLoader/BinanceTradesLoader.cs
@@ -32,10 +32,14 @@ namespace Maid.Binance
 
 		public async Task<IEnumerable<BinanceTrade>> LoadNewTrades() {
 			BinanceTrade lastTrade = GetLastTrade();
+			var currentYearDate = new DateTime(2025, 4, 1);
+			DateTime? lastTradeDate = lastTrade != null ? DateTimeOffset.FromUnixTimeMilliseconds(lastTrade.Time + 1).DateTime : null;
 			var requestParams = new LoadTradesRequest {
-				StartDate = lastTrade != null ? DateTimeOffset.FromUnixTimeMilliseconds(lastTrade.Time + 1000).DateTime : null,
+				StartDate = lastTradeDate != null ? new DateTime(lastTradeDate.Value.Year, lastTradeDate.Value.Month, lastTradeDate.Value.Day) : currentYearDate,
 				Limit = LIMIT
 			};
+			var startDate = requestParams.StartDate.Value;
+			requestParams.EndDate = new DateTime(startDate.Year, startDate.Month, startDate.Day, 23, 59, 59);
 			var allTrades = new List<BinanceTrade>();
 			while (true) {
 				var trades = await _request.RequestTrades(requestParams);
@@ -43,20 +47,17 @@ namespace Maid.Binance
 				if (trades.Any()) {
 					allTrades.AddRange(trades);
 				}
-				if (trades.Count() != LIMIT || !trades.Any()) {
+				if (requestParams.EndDate > DateTime.Now) {
 					break;
 				}
-				// By default - Binance returns trades collection starting from new trades.
-				// If my storage of trades is empty - fetch all trades from the recent ones to the oldest.
-				// If there were some trades in storage - fetch trades starting from last one and to recent ones.
-				if (lastTrade != null) {
-					var lastTradeTime = DateTimeOffset.FromUnixTimeMilliseconds(trades.Last().Time).DateTime;
-					requestParams.StartDate = lastTradeTime.AddMilliseconds(1);
-				} else {
-					var lastTradeTime = DateTimeOffset.FromUnixTimeMilliseconds(trades.First().Time).DateTime;
-					requestParams.StartDate = lastTradeTime.AddDays(-7);
-					requestParams.EndDate = lastTradeTime.AddMilliseconds(-1);
+				if (trades.Count() == LIMIT) {
+					trades = trades.OrderBy(t => t.Time);
+					requestParams.StartDate = DateTimeOffset.FromUnixTimeMilliseconds(trades.Last().Time + 1).DateTime.ToLocalTime();
+				} else { 
+					requestParams.StartDate = new DateTime(startDate.Year, startDate.Month, startDate.Day, 00, 00, 00).AddDays(1);
 				}
+				startDate = requestParams.StartDate.Value;
+				requestParams.EndDate = new DateTime(startDate.Year, startDate.Month, startDate.Day, 23, 59, 59);
 			}
 			return allTrades.OrderBy(t => t.Time);
 		}
